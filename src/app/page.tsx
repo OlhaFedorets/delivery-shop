912b461951132d075cc4af6874972360e698c862
OlhaFedorets <fedoretsolha@gmail.com>
2025-08-18 17:11:36 +0300

    change page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import Slider from "@/components/Slider/Slider";
+import Actions from "@/components/Actions";
+import NewProducts from "@/components/NewProducts";
+import Purchases from "@/components/Purchases";
+import SpecialOffers from "@/components/SpecialOffers";
 
 
 export default function Home() {
   return (
     <main className="w-full mx-auto mb-20">
       <Slider/>
+        <div className="px-[max(12px,calc((100%-1208px)/2))] flex flex-col gap-y-20 md:mb-25 xl:mb-30">
+            <Actions/>
+            <NewProducts/>
+            <Purchases/>
+            <SpecialOffers/>
+        </div>
+
     </main>
   );
 }
