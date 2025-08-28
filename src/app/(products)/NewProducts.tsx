1ca0dd02c2151c5f364616ba72e1c028e4cb77cc
OlhaFedorets <fedoretsolha@gmail.com>
2025-08-28 14:48:07 +0300

    change newProduct

--- src/app/(products)/NewProducts.tsx
+++ src/app/(products)/NewProducts.tsx
@@ -1,36 +1,19 @@
-import ProductCard from "../../components/ProductCard";
 import database from "@/data/productsDatabase.json";
-import ViewAllButton from "@/components/ViewAllButton";
+import ProductsSection from "@/components/ProductsSection";
+import {shuffleArray} from "@/utils/shuffleArray";
 
 const NewProducts = () => {
-    const newProducts = database.filter((p) =>
+    const newProducts = shuffleArray(database.filter((p) =>
         p.categories?.includes("new")
-    );
+    ));
 
     return (
-        <section>
-            <div className="flex flex-col ">
-                <div className="mb-4 md:mb-8 xl:mb-10 flex flex-row justify-between">
-                    <h2 className="text-2xl xl:text-4xl text-left font-bold text-[#414141]">
-                        Новинки
-                    </h2>
-                    <ViewAllButton btnText="All new" href="new"/>
-                </div>
-                <ul className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 xl:gap-10 justify-items-center">
-                    {newProducts.slice(0, 4).map((item, index) => (
-                        <li
-                            key={item.id}
-                            className={`${index >= 4 ? "hidden" : ""}
-            ${index >= 3 ? "md:hidden xl:block" : ""}
-            ${index >= 4 ? "xl:hidden" : ""}
-            `}
-                        >
-                            <ProductCard {...item} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
+        <ProductsSection
+            title="New"
+            viewAllButton={{text: "All new", href: "new"}}
+            products={newProducts}
+            compact
+        />
     );
 };
 
