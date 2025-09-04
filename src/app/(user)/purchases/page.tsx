f42bd12b59afa897fbc76fd791504db42b2751e3
OlhaFedorets <fedoretsolha@gmail.com>
2025-09-04 21:25:44 +0300

    fix purchases

--- src/app/(user)/purchases/page.tsx
+++ src/app/(user)/purchases/page.tsx
@@ -1,26 +1,37 @@
 import userDatabase from "@/data/userDatabase.json";
 import productsDatabase from "@/data/productsDatabase.json";
-import ProductsSection from "@/components/ProductsSection";
+import GeneralListPage from "@/app/(products)/GeneralListPage";
 
-const AllUserPurchases = () => {
+const AllUserPurchases = async ({
+                                    searchParams,
+                                }: {
+    searchParams: Promise<{ page?: string; itemsPerPage?: string }>;
+}) => {
 
     const user = userDatabase[0]
-    const priductsId = user.purchases.map((p: {id: number}) => p.id)
+    const priductsId = user.purchases.map((p: { id: number }) => p.id)
     const products = productsDatabase.filter((p) => priductsId.includes(p.id));
     if (!products) return undefined;
     products.map((product) => {
-
-        const { discountPercent, ...rest } = product;
+        const {discountPercent, ...rest} = product;
         void discountPercent;
         return rest;
     })
 
     return (
-        <ProductsSection
-            title="All purchases"
-            viewAllButton={{text:"Go to main", href:"/"}}
-            products={products}
+        <GeneralListPage
+            searchParams={searchParams}
+            props={{
+                data: products,
+                pageTitle: "All purchases",
+                basePath: "/purchases"
+            }}
         />
+        // <ProductsSection
+        //     title="All purchases"
+        //     viewAllButton={{text: "Go to main", href: "/"}}
+        //     products={products}
+        // />
     );
 };
 
