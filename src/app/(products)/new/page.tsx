eebf9a38b024302003ca751d6708205720baeaa6
OlhaFedorets <fedoretsolha@gmail.com>
2025-09-04 21:25:35 +0300

    fix new

--- src/app/(products)/new/page.tsx
+++ src/app/(products)/new/page.tsx
@@ -1,23 +1,34 @@
 import products from "@/data/productsDatabase.json";
-import ProductsSection from "@/components/ProductsSection";
-import {shuffleArray} from "@/utils/shuffleArray";
+import GeneralListPage from "@/app/(products)/GeneralListPage";
 
 export const metadata = {
     title: 'Новинки магазина "Северяночка"',
     description: 'Новые товары магазина "Северяночка"',
 };
 
-const AllNew = () => {
-    const newProducts = shuffleArray(products.filter((p) =>
+const AllNew = async ({
+                          searchParams,
+                      }: {
+    searchParams: Promise<{ page?: string; itemsPerPage?: string }>;
+}) => {
+    const newProducts = products.filter((p) =>
         p.categories.includes("new")
-    ));
+    );
 
     return (
-        <ProductsSection
-            title="New"
-            viewAllButton={{text: "Go to main", href: "/"}}
-            products={newProducts}
+        <GeneralListPage
+            searchParams={searchParams}
+            props={{
+                data: newProducts,
+                pageTitle: "New",
+                basePath: "/new"
+            }}
         />
+        // <ProductsSection
+        //     title="New"
+        //     viewAllButton={{text: "Go to main", href: "/"}}
+        //     products={newProducts}
+        // />
     );
 };
 
