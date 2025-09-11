3b6b26e174121509c288de6338bc9b883025900a
OlhaFedorets <fedoretsolha@gmail.com>
2025-09-11 12:30:19 +0300

    fix ArticlesSection

--- src/app/(articles)/ArticlesSection.tsx
+++ src/app/(articles)/ArticlesSection.tsx
@@ -12,10 +12,12 @@ const ArticlesSection = ({
     return (
         <section>
             <div
-                className={`flex flex-col text-[#414141] ${!compact ? "px-[max(12px,calc((100%-1208px)/2))] mt-20" : ""}`}>
+                className={`flex flex-col text-[#414141] ${!compact ? "px-[max(12px,calc((100%-1208px)/2))]" : "mt-20"}`}>
                 <div className="mb-4 md:mb-8 xl:mb-10 flex flex-row justify-between">
                     <h2 className="text-2xl xl:text-4xl text-left font-bold">{title}</h2>
+                    {viewAllButton && (
                     <ViewAllButton btnText={viewAllButton.text} href={viewAllButton.href}/>
+                        )}
                 </div>
 
                  {/* Список статей */}
