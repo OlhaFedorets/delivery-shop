eefbb694f760b7f8d9d649e5e7c73cd67a17bf7a
OlhaFedorets <fedoretsolha@gmail.com>
2025-09-11 12:31:11 +0300

    fix layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {Rubik} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/components/Footer";
+import Breadcrumbs from "@/components/Breadcrumbs";
 
 const rubik = Rubik({
     variable: "--font-rubik",
@@ -26,6 +27,7 @@ export default function RootLayout({
             className={`${rubik.variable} font-sans`}
         >
         <Header/>
+        <Breadcrumbs/>
         {children}
         <Footer/>
         </body>
