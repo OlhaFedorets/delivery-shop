a40751d9adc080bb8575a7a48a03e8d040b2f813
OlhaFedorets <fedoretsolha@gmail.com>
2025-09-12 16:04:59 +0300

    fix buttonSearch

--- src/components/header/ButtonSearch.tsx
+++ src/components/header/ButtonSearch.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const ButtonSearch = () => {
     return (
+        <Link href='./catalog' className="bg-(--color-primary) hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active) hidden md:flex w-10 p-2 gap-4 lg:w-35 cursor-pointer duration-300 rounded">
+            <Image
+                src="/icons-header/icon-menu.svg"
+                alt="menu"
+                width={24}
+                height={24}
+                className="hidden md:block"
+            />
+            <span className="text-base text-white hidden lg:block">
+        Каталог
+      </span>
+        </Link>
+    );
+};
 
-        <button className="bg-(--color-primary) hover:shadow-(--shadow-buttton-default)
-            active:shadow-(--shadow-buttton-active) hidden md:flex w-10 p-2 gap-4 lg:w-35 cursor-pointer
-            duration-300 rounded">
-            <Image src="/icons-header/icon-menu.svg" alt="Menu" width="24" height="24"
-                   className="hidden md:block"/>
-            <span className="text-base text-white hidden lg:block">Catalog</span>
-        </button>
-    )
-}
-
-export default ButtonSearch
+export default ButtonSearch;
