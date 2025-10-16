f84062fba2e0c99c47a9cc4e86e876cf30980c4f
OlhaFedorets <fedoretsolha@gmail.com>
2025-10-16 14:27:02 +0300

    change buttonSearch

--- src/components/header/ButtonSearch.tsx
+++ src/components/header/ButtonSearch.tsx
@@ -3,20 +3,20 @@ import Link from "next/link";
 
 const ButtonSearch = () => {
     return (
-        <Link
-            href="/catalog"
-            className="bg-(--color-primary) hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active) hidden md:flex w-10 p-2 gap-4 lg:w-35 cursor-pointer duration-300 rounded">
-            <Image
-                src="/icons-header/icon-menu.svg"
-                alt="menu"
-                width={24}
-                height={24}
-                className="hidden md:block"
-            />
-            <span className="text-base text-white hidden lg:block">
-        Каталог
-      </span>
-        </Link>
+            <Link
+                href="/catalog"
+                className="bg-(--color-primary) hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active)
+                flex items-center p-2 gap-2 lg:gap-4 lg:px-4 lg:w-35 cursor-pointer duration-300 rounded">
+                <Image
+                    src="/icons-header/icon-menu.svg"
+                    alt="menu"
+                    width={24}
+                    height={24}
+                />
+                <span className="text-base text-white hidden md:block">
+                Каталог
+            </span>
+            </Link>
     );
 };
 
