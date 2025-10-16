1b3e0af375dd0b768365a5854ac56a81a03c2dff
OlhaFedorets <fedoretsolha@gmail.com>
2025-10-16 14:27:30 +0300

    change logoBlock

--- src/components/header/LogoBlock.tsx
+++ src/components/header/LogoBlock.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 const LogoBlock = () => {
     return (
+
         <Link href='/' className="flex flex-row gap-3 items-center cursor-pointer">
         <div className="relative w-10 h-8 md:w-12 md:h-10 xl:w-10 xl:h-8">
             <Image src="/icons-header/icon-logo.svg" alt="Logo" fill sizes="(max-width:768px) 100vw,
@@ -12,6 +13,7 @@ const LogoBlock = () => {
                 <Image src="/icons-header/icon-text.png" alt="Северяночка" fill sizes="100px"/>
             </div>
         </Link>
+
     )
 }
 
