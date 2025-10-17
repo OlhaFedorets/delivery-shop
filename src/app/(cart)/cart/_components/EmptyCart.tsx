dceac33221e43291cff5ae363ecd5152180e3dd1
OlhaFedorets <fedoretsolha@gmail.com>
2025-10-17 15:16:50 +0300

    change emptyCart

--- src/app/(cart)/cart/_components/EmptyCart.tsx
+++ src/app/(cart)/cart/_components/EmptyCart.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 const EmptyCart = () => {
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-2xl font-bold mb-8">Корзина</h1>
+            <h1 className="text-4xl md:text-5xl xl:text-[64px] font-bold mb-8 md:mb-10 xl:mb-15">
+                Корзина
+            </h1>
             <div className="text-center py-12">
                 <p className="text-gray-500 mb-4">Ваша корзина пуста</p>
                 <Link
