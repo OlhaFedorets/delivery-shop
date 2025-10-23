119cbf3592556deddf551731a24ed4ba5f1fb9ff
OlhaFedorets <fedoretsolha@gmail.com>
2025-10-23 17:14:08 +0300

    change category

--- src/app/(catalog)/catalog/[category]/page.tsx
+++ src/app/(catalog)/catalog/[category]/page.tsx
@@ -37,9 +37,9 @@ const CategoryPage = async ({
 
     return (
         <div className="px-[max(12px,calc((100%-1208px)/2))] flex flex-col mx-auto">
-            <h1 className="ml-3 xl:ml-0 text-4xl md:text-5xl text-left font-bold text-main-text mb-8 md:mb-10 xl:mb-15 max-w-[336px] md:max-w-max leading-[150%]">
-                {TRANSLATIONS[category] || category}
-            </h1>
+            {/*<h1 className="ml-3 xl:ml-0 text-4xl md:text-5xl text-left font-bold text-main-text mb-8 md:mb-10 xl:mb-15 max-w-[336px] md:max-w-max leading-[150%]">*/}
+            {/*    {TRANSLATIONS[category] || category}*/}
+            {/*</h1>*/}
             <GeneralListPage
                 searchParams={Promise.resolve(resolvedSearchParams)}
                 props={{
