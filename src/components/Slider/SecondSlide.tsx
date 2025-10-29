643a1fde5d6c51f5b7166aff5839f71a32909b5e
OlhaFedorets <fedoretsolha@gmail.com>
2025-10-29 14:14:24 +0300

    change secondSlide

--- src/components/Slider/SecondSlide.tsx
+++ src/components/Slider/SecondSlide.tsx
@@ -1,11 +1,30 @@
+// import Image from "next/image";
+//
+// const SecondSlide = () => {
+//     return (
+//         <div className="mb-10 md:mb-15 xl:mb-20 h-20 md:h-40 xl:h-50 w-full relative">
+//             <Image src="/images/graphics/slide-2.jpg" alt="Slide" fill priority className="object-cover" />
+//         </div>
+//     );
+// }
+//
+// export default SecondSlide
+
 import Image from "next/image";
 
 const SecondSlide = () => {
     return (
-        <div className="mb-10 md:mb-15 xl:mb-20 h-20 md:h-40 xl:h-50 w-full relative">
-            <Image src="/images/graphics/slide-2.jpg" alt="Slide" fill priority className="object-cover" />
+        <div className="mb-10 md:mb-15 xl:mb-20 w-full relative">
+            <Image
+                src="/images/graphics/slide-2.png"
+                alt="Slide"
+                width={800}
+                height={200}
+                className="object-cover w-auto h-auto mx-auto"
+                priority
+            />
         </div>
     );
 }
 
-export default SecondSlide
+export default SecondSlide;
