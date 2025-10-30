b2ff101e0878cdeb542b6befafeddc0a8a9491ab
OlhaFedorets <fedoretsolha@gmail.com>
2025-10-30 18:22:51 +0300

    change ButtonSearch

--- src/components/header/ButtonSearch.tsx
+++ src/components/header/ButtonSearch.tsx
@@ -5,8 +5,8 @@ const ButtonSearch = () => {
     return (
             <Link
                 href={"/catalog"}
-                className="bg-(--color-primary) hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active)
-                flex items-center p-2 gap-2 lg:gap-4 lg:px-4 lg:w-35 cursor-pointer duration-300 rounded">
+                className="bg-(--color-primary) hover:brightness-130 active:shadow-(--shadow-button-active)
+                flex items-center p-2 mt-5 mb-5 gap-2 lg:gap-4 lg:px-4 lg:w-35 cursor-pointer duration-300 rounded">
                 <Image
                     src="/icons-header/icon-menu.svg"
                     alt="menu"
