468087d109ca83e5168370fdd93a87d8d32defc2
OlhaFedorets <fedoretsolha@gmail.com>
2025-10-30 18:25:10 +0300

    change DiscountMarker

--- src/app/(cart)/cart/_components/DiscountMarker.tsx
+++ src/app/(cart)/cart/_components/DiscountMarker.tsx
@@ -20,7 +20,7 @@ const DiscountMarker = () => {
                     className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full
                     peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px]
                     after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all
-                    peer-checked:bg-[#ff6633]"></div>
+                    peer-checked:bg-[var(--color-primary)]"></div>
             </label>
         </div>
     )
