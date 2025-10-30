105b3cd9968dfbc994dd2b3f22aa34df66ae4d67
OlhaFedorets <fedoretsolha@gmail.com>
2025-10-30 18:22:38 +0300

    change adToCartButton

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { useState } from "react";
-import { useCartStore } from "@/store/cartStore";
 
-const AddToCartButton = ({ productId }: { productId: string }) => {
-    // const [isLoading, setIsLoading] = useState(false);
-    const { cartItems, updateCart } = useCartStore();
+import {useCartStore} from "@/store/cartStore";
+
+const AddToCartButton = ({productId}: { productId: string }) => {
+    const {cartItems, updateCart} = useCartStore();
 
     const handleSubmit = async () => {
-        // setIsLoading(true);
-
         // Проверяем, есть ли товар уже в корзине
         const existingItem = cartItems.find(
             (item) => item.productId === productId
@@ -19,7 +16,7 @@ const AddToCartButton = ({ productId }: { productId: string }) => {
             // Если товар уже есть, увеличиваем количество
             const updatedItems = cartItems.map(item =>
                 item.productId === productId
-                    ? { ...item, quantity: item.quantity + 1 }
+                    ? {...item, quantity: item.quantity + 1}
                     : item
             );
             updateCart(updatedItems);
@@ -33,8 +30,6 @@ const AddToCartButton = ({ productId }: { productId: string }) => {
             const newCartItems = [...cartItems, newCartItem];
             updateCart(newCartItems);
         }
-
-        // setIsLoading(false);
     };
 
     return (
@@ -42,8 +37,9 @@ const AddToCartButton = ({ productId }: { productId: string }) => {
             <form action={handleSubmit}>
                 <button
                     type="submit"
-                    // disabled={isLoading}
-                    className="absolute border bottom-2 left-2 right-2 border-primary hover:text-white hover:bg-[#ff6633] hover:border-transparent active:shadow-(--shadow-button-active) h-10 rounded justify-center items-center text-primary transition-all duration-300 cursor-pointer select-none"
+                    className="absolute border bottom-2 left-2 right-2 border-primary hover:text-white hover:bg-[var(--color-secondary)]
+                    hover:border-transparent active:shadow-(--shadow-button-active) h-10 rounded justify-center items-center text-primary
+                    transition-all duration-300 cursor-pointer select-none"
                 >
                     В корзину
                 </button>
@@ -53,63 +49,3 @@ const AddToCartButton = ({ productId }: { productId: string }) => {
 };
 
 export default AddToCartButton;
-
-
-// "use client";
-//
-// import { useState } from "react";
-// import { useCartStore } from "@/store/cartStore";
-// import {CartItem} from "@/types/cart";
-//
-// const AddToCartButton = ({ productId }: { productId: string }) => {
-//     // const [isLoading, setIsLoading] = useState(false);
-//     // const [message, setMessage] = useState<{
-//     //     success: boolean;
-//     //     message: string;
-//     // } | null>(null);
-//
-//     const { fetchCart } = useCartStore();
-//
-//     const handleSubmit = async () => {
-//         // setIsLoading(true);
-//         // setMessage(null);
-//
-//         const cartItems: CartItem[] = [];
-//
-//         const existingItem = cartItems.find(
-//             (item: CartItem) => item.productId === productId
-//         );
-//
-//         if (existingItem) {
-//             return {
-//                 success: false,
-//                 message:
-//                     "Товар уже в корзине. Изменить его количество можно на странице корзины",
-//             };
-//         }
-//
-//
-//         const newCartItem: CartItem = {
-//             productId,
-//             quantity: 1,
-//             addedAt: new Date(),
-//         };
-//
-//         const newCartItems = [...cartItems, newCartItem];
-//         fetchCart()
-//     return (
-//         <div className="relative">
-//             <form action={handleSubmit}>
-//                 <button
-//                     type="submit"
-//                     // disabled={isLoading}
-//                     className="absolute border bottom-2 left-2 right-2 border-primary hover:text-white hover:bg-[#ff6633] hover:border-transparent active:shadow-(--shadow-button-active) h-10 rounded justify-center items-center text-primary transition-all duration-300 cursor-pointer select-none"
-//                 >
-//                     В корзину
-//                 </button>
-//             </form>
-//         </div>
-//     );
-// };
-//
-// export default AddToCartButton;
