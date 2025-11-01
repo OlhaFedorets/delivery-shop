81ce8f3b439e7afe6209d605f87b806939211453
OlhaFedorets <fedoretsolha@gmail.com>
2025-11-01 15:07:35 +0300

    change itemPrice

--- src/app/(cart)/cart/_components/ItemPrice.tsx
+++ src/app/(cart)/cart/_components/ItemPrice.tsx
@@ -12,23 +12,23 @@ const ItemPrice = ({item, finalPrice, cardPrice}: ItemPriceProps) => {
         <>
             <div className="flex items-center space-x-2 mt-2">
                 {item.product.discountPercent > 0 && (
-                    <span className="text-gray-400 line-through text-sm">
+                    <span className="text-[var(--color-tertiary)] line-through text-sm">
                         {item.product.basePrice} руб.
                     </span>
                 )}
-                <span className="text-[#ff6633] font-semibold">
+                <span className="text-[var(--color-secondary)] font-semibold">
                     {finalPrice.toFixed(2)} руб.
                 </span>
                 {item.product.discountPercent > 0 && (
                     <span
-                        className="bg-red-100 text-red-600 px-2 py-1 rounded text-xs">
+                        className="bg-red-100 text-red-700 px-2 py-1 rounded text-xs">
                         -{item.product.discountPercent}%
                     </span>
                 )}
             </div>
 
             <div className="mt-1">
-                <span className="text-green-600 text-sm font-semibold">
+                <span className="text-[var(--color-tertiary)] text-sm font-semibold">
                     {cardPrice.toFixed(2)} руб. с картой
                 </span>
             </div>
