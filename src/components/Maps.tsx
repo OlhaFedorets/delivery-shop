8aa4afab93a6c74b7ee4429d28e469f24818c413
OlhaFedorets <fedoretsolha@gmail.com>
2025-11-06 17:58:47 +0300

    change maps

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { YMaps, Map, Placemark } from "@iminside/react-yandex-maps";
-import { useState } from "react";
-import { locations } from "@/data/locations";
+import {YMaps, Map, Placemark} from "@iminside/react-yandex-maps";
+import {useState} from "react";
+import {locations} from "@/data/locations";
 
 const Maps = () => {
     const [currentLocation, setCurrentLocation] = useState("saintPetersburg");
@@ -16,55 +16,53 @@ const Maps = () => {
                 load: "package.full",
             }}
         >
-            <section>
-                <div className="flex flex-col justify-center xl:max-w-[1208px] text-[#414141]">
-                    <h2 className="mb-4 md:mb-8 xl:mb-10 text-2xl xl:text-4xl text-left font-bold">
-                        Наши магазины
-                    </h2>
-                    <div className="flex flex-wrap gap-x-2 gap-y-3 mb-5">
-                        {Object.keys(locations).map((key) => {
-                            const isActive = currentLocation === key;
-                            return (
-                                <button
-                                    key={key}
-                                    onClick={() => setCurrentLocation(key)}
-                                    className={`p-2 text-xs justify-center items-center active:shadow-(--shadow-button-active) 
+            <section className="flex flex-col justify-center xl:max-w-[1208px] text-[#414141]">
+                <h2 className="mb-4 md:mb-8 xl:mb-10 text-2xl xl:text-4xl text-left font-bold">
+                    Наши магазины
+                </h2>
+                <div className="flex flex-wrap gap-x-2 gap-y-3 mb-5">
+                    {Object.keys(locations).map((key) => {
+                        const isActive = currentLocation === key;
+                        return (
+                            <button
+                                key={key}
+                                onClick={() => setCurrentLocation(key)}
+                                className={`p-2 text-xs justify-center items-center active:shadow-(--shadow-button-active) 
                                     border-none rounded cursor-pointer transition-colors duration-300 ${
-                                        isActive
-                                            ? "bg-(--color-primary) text-white hover: shadow-(--shadow-default)"
-                                            : "bg-[#f3f2f1] hover:shadow-(--shadow-button-secondary)"
-                                    }`}
-                                >
-                                    {locations[key].name}
-                                </button>
-                            );
-                        })}
-                    </div>
-                    <Map
-                        // options={{ suppressMapOpenBlock: true }}
-                        defaultState={{ center: currentLocationData.center, zoom: 12 }}
-                        state={{ center: currentLocationData.center, zoom: 12 }}
-                        width="100%"
-                        height="354px"
-                        // className="no-ymaps-copyright w-full h-[354px]"
-                    >
-                        {locations[currentLocation].shops.map((shop) => (
-                            <Placemark
-                                key={shop.id}
-                                geometry={shop.coordinates}
-                                properties={{
-                                    hintContent: shop.name
-                                }}
-                                options={{
-                                    iconLayout: "default#image",
-                                    iconImageHref: "/icons-map/icon-location.svg",
-                                    iconImageSize: [32, 32],
-                                    iconOffset: [-16, -16],
-                                }}
-                            />
-                        ))}
-                    </Map>
+                                    isActive
+                                        ? "bg-(--color-primary) text-white hover: shadow-(--shadow-default)"
+                                        : "bg-[#f3f2f1] hover:shadow-(--shadow-button-secondary)"
+                                }`}
+                            >
+                                {locations[key].name}
+                            </button>
+                        );
+                    })}
                 </div>
+                <Map
+                    // options={{ suppressMapOpenBlock: true }}
+                    defaultState={{center: currentLocationData.center, zoom: 12}}
+                    state={{center: currentLocationData.center, zoom: 12}}
+                    width="100%"
+                    height="354px"
+                    // className="no-ymaps-copyright w-full h-[354px]"
+                >
+                    {locations[currentLocation].shops.map((shop) => (
+                        <Placemark
+                            key={shop.id}
+                            geometry={shop.coordinates}
+                            properties={{
+                                hintContent: shop.name
+                            }}
+                            options={{
+                                iconLayout: "default#image",
+                                iconImageHref: "/icons-map/icon-location.svg",
+                                iconImageSize: [32, 32],
+                                iconOffset: [-16, -16],
+                            }}
+                        />
+                    ))}
+                </Map>
             </section>
         </YMaps>
     );
