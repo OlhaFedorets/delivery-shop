6ea6308c186c798fcababef5a12528c7a7d17c3b
OlhaFedorets <fedoretsolha@gmail.com>
2025-11-11 21:08:05 +0300

    change  bonuses

--- src/app/(catalog)/catalog/[category]/(productPage)/[id]/_components/Bonuses.tsx
+++ src/app/(catalog)/catalog/[category]/(productPage)/[id]/_components/Bonuses.tsx
@@ -1,22 +1,16 @@
-import Image from "next/image";
-import { getBonusesWord } from "@/utils/bonusWord";
-const Bonuses = ({ bonus }: { bonus: number }) => {
+import {getBonusesWord} from "@/utils/bonusWord";
+
+const Bonuses = ({bonus}: { bonus: number }) => {
     const roundedBonus = Math.round(bonus);
     const bonusWord = getBonusesWord(roundedBonus);
 
     return (
         <div className="w-[212px] flex flex-row gap-x-2 items-center justify-center mx-auto mb-2">
-            <Image
-                src="/icons-products/icon-green-smile.svg"
-                alt="Бонусы"
-                width={24}
-                height={11}
-            />
             <p className="text-xs text-primary">
                 Вы получаете{" "}
                 <span className="font-bold">
-          {roundedBonus} {bonusWord}
-        </span>
+                    {roundedBonus} {bonusWord}
+                </span>
             </p>
         </div>
     );
