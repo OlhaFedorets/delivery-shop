e38875f7b707ceda8ae4bf3c27756c009a29601c
OlhaFedorets <fedoretsolha@gmail.com>
2025-11-13 10:45:32 +0300

    change category

--- src/app/(catalog)/catalog/[category]/page.tsx
+++ src/app/(catalog)/catalog/[category]/page.tsx
@@ -12,7 +12,7 @@ export async function generateMetadata({
         title: TRANSLATIONS[category] || category,
         description: `Описание категории товаров "${
             TRANSLATIONS[category] || category
-        }" магазина "Северяночка"`,
+        }" магазина "Mypetshop"`,
     };
 }
 
@@ -33,19 +33,15 @@ const CategoryPage = async ({
     const {category} = await params;
     const resolvedSearchParams = await searchParams;
 
-    const data = productsData.filter(p => p.categories[0] === category);
+    const data = productsData.filter(p => p.category === category);
 
     return (
         <div className="px-[max(12px,calc((100%-1208px)/2))] flex flex-col mx-auto">
-            {/*<h1 className="ml-3 xl:ml-0 text-4xl md:text-5xl text-left font-bold text-main-text mb-8 md:mb-10 xl:mb-15 max-w-[336px] md:max-w-max leading-[150%]">*/}
-            {/*    {TRANSLATIONS[category] || category}*/}
-            {/*</h1>*/}
             <GeneralListPage
                 searchParams={Promise.resolve(resolvedSearchParams)}
                 props={{
                     data: data,
                     pageTitle: TRANSLATIONS[category] || category,
-
                     basePath: `/catalog/${category}`,
                     contentType: "category",
                 }}
