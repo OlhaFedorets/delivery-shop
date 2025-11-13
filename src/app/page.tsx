c2c8ccee8e220d6729d4649ecc41862856cc6257
OlhaFedorets <fedoretsolha@gmail.com>
2025-11-13 10:45:40 +0300

    change app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,24 @@
-import Slider from "@/components/Slider/Slider";
+import Slider from "@/components/slider/Slider";
 import Actions from "@/app/(products)/Actions";
 import NewProducts from "@/app/(products)/NewProducts";
-import Purchases from "@/app/(user)/Purchases";
+import Popular from "@/app/(products)/Popular";
 import SpecialOffers from "@/components/SpecialOffers";
 import Maps from "@/components/Maps";
 import Articles from "@/app/(articles)/Articles";
 
 
 export default function Home() {
-  return (
-    <main className="w-full mx-auto mb-20">
-      <Slider/>
-        <div className="px-[max(12px,calc((100%-1208px)/2))] flex flex-col gap-y-20 md:mb-25 xl:mb-30">
-            <Actions/>
-            <NewProducts/>
-            <Purchases/>
-            <SpecialOffers/>
-            <Maps/>
-            <Articles/>
-        </div>
-
-    </main>
-  );
+    return (
+        <main className="w-full mx-auto mb-20">
+            <Slider/>
+            <div className="px-[max(12px,calc((100%-1208px)/2))] flex flex-col gap-y-20 md:mb-25 xl:mb-30">
+                <Actions/>
+                <NewProducts/>
+                <Popular/>
+                <SpecialOffers/>
+                <Maps/>
+                <Articles/>
+            </div>
+        </main>
+    );
 }
