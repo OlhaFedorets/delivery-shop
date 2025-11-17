c0490a43b795ebf143eab321081bcbf8ae3efa7f
OlhaFedorets <fedoretsolha@gmail.com>
2025-11-17 14:28:36 +0300

    change GeneralListPage

--- src/app/(favourites)/favourites/page.tsx
+++ src/app/(favourites)/favourites/page.tsx
@@ -37,7 +37,7 @@ const FavoritesPage = () => {
             </div>
 
             <ul className={`grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-6 xl:gap-10 justify-items-center`}>
-                {favouriteProducts.map((item, index) => (
+                {favouriteProducts.map((item) => (
                     <li key={item.id}>
                         <ProductCard {...item} />
                     </li>
